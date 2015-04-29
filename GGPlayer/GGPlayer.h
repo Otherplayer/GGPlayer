7709e9f65f892e8140d97159402575b918288967
zero <osmk@qq.com>
2015-04-29 21:18:47 +0800

    build

---
+++ GGPlayer/GGPlayer.h
@@ -0,0 +1,13 @@
+//
+//  GGPlayer.h
+//  GGPlayer
+//
+//  Created by __无邪_ on 15/4/25.
+//  Copyright (c) 2015年 __无邪_. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GGPlayer : UIView
+
+@end
